384d559ab2be91b915a35f8007838ba87f7fd20b
lgreenhill <Rabbit@456>
2020-12-21 14:05:04 -0500

    added platformio config files

---
+++ Main/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  Serial.print("Hello");
+  // put your main code here, to run repeatedly:
+}
