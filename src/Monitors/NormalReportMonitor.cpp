22b3fd62baaa7093e76e7555742ae09c90668c21
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-06-10 20:22:57 -0400

    fix fault monitor/normal report

--- src/Monitors/NormalReportMonitor.cpp
+++ src/Monitors/NormalReportMonitor.cpp
@@ -203,28 +203,74 @@ void NormalReportMonitor::execute(){
     Serial.println(sfr::button::pressed);
     #endif
     
-    uint8_t mag_x = map(sfr::imu::mag_x, constants::imu::min_mag_x, constants::imu::max_mag_x, 0, 255);
-    uint8_t mag_y = map(sfr::imu::mag_y, constants::imu::min_mag_y, constants::imu::max_mag_y, 0, 255);
-    uint8_t mag_z = map(sfr::imu::mag_z, constants::imu::min_mag_z, constants::imu::max_mag_z, 0, 255);
+    uint8_t mag_x = map(sfr::imu::mag_x_average, constants::imu::min_mag_x, constants::imu::max_mag_x, 0, 255);
+    uint8_t mag_y = map(sfr::imu::mag_y_average, constants::imu::min_mag_y, constants::imu::max_mag_y, 0, 255);
+    uint8_t mag_z = map(sfr::imu::mag_z_average, constants::imu::min_mag_z, constants::imu::max_mag_z, 0, 255);
 
-    uint8_t gyro_x = map(sfr::imu::gyro_x, constants::imu::min_gyro_x, constants::imu::max_gyro_x, 0, 255);
-    uint8_t gyro_y = map(sfr::imu::gyro_y, constants::imu::min_gyro_y, constants::imu::max_gyro_y, 0, 255);
-    uint8_t gyro_z = map(sfr::imu::gyro_z, constants::imu::min_gyro_z, constants::imu::max_gyro_z, 0, 255);
+    uint8_t gyro_x = map(sfr::imu::gyro_x_average, constants::imu::min_gyro_x, constants::imu::max_gyro_x, 0, 255);
+    uint8_t gyro_y = map(sfr::imu::gyro_y_average, constants::imu::min_gyro_y, constants::imu::max_gyro_y, 0, 255);
+    uint8_t gyro_z = map(sfr::imu::gyro_z_average, constants::imu::min_gyro_z, constants::imu::max_gyro_z, 0, 255);
 
-    uint8_t acc_x = map(sfr::imu::acc_x, constants::imu::min_acc_x, constants::imu::max_acc_x, 0, 255);
-    uint8_t acc_y = map(sfr::imu::acc_y, constants::imu::min_acc_y, constants::imu::max_acc_y, 0, 255);
-    uint8_t acc_z = map(sfr::imu::acc_z, constants::imu::min_acc_z, constants::imu::max_acc_z, 0, 255);
+    uint8_t temp_c = map(sfr::temperature::temp_c_average, constants::temperature::min_temp_c, constants::temperature::max_temp_c, 0, 255);
 
-    uint8_t report[70] = {mag_x, 
-                        mag_y, 
-                        mag_z, 
-                        gyro_x, 
-                        gyro_y, 
-                        gyro_z, 
-                        acc_x, 
-                        acc_y, 
-                        acc_z, 
-                        sfr::button::pressed, 
-                        sfr::fault::fault_1};
-    
+    uint8_t solar_current = map(sfr::current::solar_current_average, constants::current::min_solar_current, constants::current::max_solar_current, 0, 255);
+
+    uint8_t voltage = map(sfr::battery::voltage_average, constants::battery::min_voltage, constants::battery::max_voltage, 0, 255);
+
+    uint8_t report[70] = {
+        sfr::photoresistor::covered,
+
+        (int)sfr::mission::mode,
+
+        sfr::burnwire::fire,
+        sfr::burnwire::arm,
+        (int)sfr::burnwire::mode,
+        sfr::burnwire::attempts,
+
+        sfr::rockblock::downlink_period,
+        (int)sfr::rockblock::mode,
+        sfr::rockblock::waiting_message,
+        sfr::rockblock::waiting_command,
+        sfr::rockblock::f_opcode,
+        sfr::rockblock::f_arg_1,
+        sfr::rockblock::f_arg_2,
+
+        mag_x,
+        mag_y,
+        mag_z,
+        gyro_x,
+        gyro_y,
+        gyro_z,
+
+        temp_c,
+        (int)sfr::temperature::mode,
+
+        solar_current,
+        sfr::current::in_sun,
+
+        (int)sfr::acs::mode,
+
+        voltage,
+
+        (int)sfr::fault::mode,
+
+        sfr::button::pressed,
+
+        sfr::fault::check_mag_x,
+        sfr::fault::check_mag_y,
+        sfr::fault::check_mag_z,
+        sfr::fault::check_gyro_x,
+        sfr::fault::check_gyro_y,
+        sfr::fault::check_gyro_z,
+        sfr::fault::check_acc_x,
+        sfr::fault::check_acc_y,
+
+        sfr::fault::check_acc_z,
+        sfr::fault::check_temp_c,
+        sfr::fault::check_solar_current,
+        sfr::fault::check_voltage,
+
+        sfr::camera::take_photo,
+        sfr::camera::powered
+    };   
 }
