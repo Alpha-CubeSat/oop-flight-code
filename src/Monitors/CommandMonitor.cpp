00f88a9b9d7c601a73200069d2acd0167109b7a5
hwm44 <hwm44@cornell.edu>
2021-06-24 23:06:14 -0400

    Cleaned up command monitor and sfr/const types

--- src/Monitors/CommandMonitor.cpp
+++ src/Monitors/CommandMonitor.cpp
@@ -4,114 +4,113 @@ CommandMonitor::CommandMonitor(unsigned int offset): TimedControlTask<void>(offs
 
 void CommandMonitor::execute(){
     if(sfr::rockblock::waiting_command){
-        if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::mission_mode[0], constants::rockblock::mission_mode[1])){
+        if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::mission_mode)){
             dispatch_change_mission_mode();
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::burnwire_arm[0], constants::rockblock::burnwire_arm[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::burnwire_arm)){
             dispatch_change_true_false(sfr::burnwire::arm);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::burnwire_fire[0], constants::rockblock::burnwire_fire[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::burnwire_fire)){
             dispatch_change_true_false(sfr::burnwire::fire);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::camera_take_photo[0], constants::rockblock::camera_take_photo[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::camera_take_photo)){
             dispatch_change_true_false(sfr::camera::take_photo);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::temperature_mode[0], constants::rockblock::temperature_mode[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::temperature_mode)){
             dispatch_change_temperature_mode();
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::acs_mode[0], constants::rockblock::acs_mode[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::acs_mode)){
             dispatch_change_acs_mode();
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_mode[0], constants::rockblock::fault_mode[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_mode)){
             dispatch_change_fault_mode();
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_mag_x[0], constants::rockblock::fault_check_mag_x[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_mag_x)){
             dispatch_change_true_false(sfr::fault::check_mag_x);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_mag_y[0], constants::rockblock::fault_check_mag_y[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_mag_y)){
             dispatch_change_true_false(sfr::fault::check_mag_y);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_mag_z[0], constants::rockblock::fault_check_mag_z[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_mag_z)){
             dispatch_change_true_false(sfr::fault::check_mag_z);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_gyro_x[0], constants::rockblock::fault_check_gyro_x[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_gyro_x)){
             dispatch_change_true_false(sfr::fault::check_gyro_x);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_gyro_y[0], constants::rockblock::fault_check_gyro_y[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_gyro_y)){
             dispatch_change_true_false(sfr::fault::check_gyro_y);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_gyro_z[0], constants::rockblock::fault_check_gyro_z[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_gyro_z)){
             dispatch_change_true_false(sfr::fault::check_gyro_z);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_temp_c[0], constants::rockblock::fault_check_temp_c[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_temp_c)){
             dispatch_change_true_false(sfr::fault::check_temp_c);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_solar_current[0], constants::rockblock::fault_check_solar_current[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_solar_current)){
             dispatch_change_true_false(sfr::fault::check_solar_current);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_voltage[0], constants::rockblock::fault_check_voltage[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::fault_check_voltage)){
             dispatch_change_true_false(sfr::fault::check_voltage);
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::request_image_fragment[0], constants::rockblock::request_image_fragment[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::request_image_fragment)){
             dispatch_request_image_fragment();
         }
-        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::rockblock_downlink_period[0], constants::rockblock::rockblock_downlink_period[1])){
+        else if(sfr::rockblock::f_opcode == get_decimal_opcode(constants::rockblock::rockblock_downlink_period)){
             dispatch_change_rockblock_downlink_period();
-        }
-        else{
+        } else{
             sfr::rockblock::waiting_command = false;
         }
     }
 }
 
 void CommandMonitor::dispatch_change_mission_mode(){
-    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::deployment[0], constants::rockblock::deployment[1], constants::rockblock::deployment[2], constants::rockblock::deployment[3])){
+    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::deployment)){
         MissionManager::transition_to_deployment();
     }
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::standby[0], constants::rockblock::standby[1], constants::rockblock::standby[2], constants::rockblock::standby[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::standby)){
         MissionManager::transition_to_standby();
     }
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::safe[0], constants::rockblock::safe[1], constants::rockblock::safe[2], constants::rockblock::safe[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::safe)){
         MissionManager::transition_to_safe();
     }
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::low_power[0], constants::rockblock::low_power[1], constants::rockblock::low_power[2], constants::rockblock::low_power[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::low_power)){
         MissionManager::transition_to_low_power();
     }
     sfr::rockblock::waiting_command = false;
 }
 
-void CommandMonitor::dispatch_change_true_false(bool value){
+void CommandMonitor::dispatch_change_true_false(bool& value){
     value = sfr::rockblock::f_arg_1;
     sfr::rockblock::waiting_command = false;
 }
 
 void CommandMonitor::dispatch_change_temperature_mode(){
-    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::active[0],constants::rockblock::active[1],constants::rockblock::active[2],constants::rockblock::active[3])){
+    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::active)){
         sfr::temperature::mode = temp_mode_type::active;
     } 
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::inactive[0],constants::rockblock::inactive[1],constants::rockblock::inactive[2],constants::rockblock::inactive[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::inactive)){
         sfr::temperature::mode = temp_mode_type::inactive;
     }
     sfr::rockblock::waiting_command = false;
 }
 void CommandMonitor::dispatch_change_acs_mode(){
-    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::detumbling[0],constants::rockblock::detumbling[1],constants::rockblock::detumbling[2],constants::rockblock::detumbling[3])){
+    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::detumbling)){
         sfr::acs::mode = acs_mode_type::detumble;
     } 
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::pointing[0],constants::rockblock::pointing[1],constants::rockblock::pointing[2],constants::rockblock::pointing[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::pointing)){
         sfr::acs::mode = acs_mode_type::point;
     }
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::off[0],constants::rockblock::off[1],constants::rockblock::off[2],constants::rockblock::off[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::off)){
         sfr::acs::mode = acs_mode_type::off;
     }
     sfr::rockblock::waiting_command = false;
 }
 
 void CommandMonitor::dispatch_change_fault_mode(){
-    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::active[0],constants::rockblock::active[1],constants::rockblock::active[2],constants::rockblock::active[3])){
+    if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::active)){
         sfr::temperature::mode = temp_mode_type::active;
     } 
-    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::inactive[0],constants::rockblock::inactive[1],constants::rockblock::inactive[2],constants::rockblock::inactive[3])){
+    else if(sfr::rockblock::f_arg_1 == get_decimal_arg(constants::rockblock::inactive)){
         sfr::temperature::mode = temp_mode_type::inactive;
     }
     sfr::rockblock::waiting_command = false;
@@ -129,22 +128,10 @@ void CommandMonitor::dispatch_change_rockblock_downlink_period(){
     sfr::rockblock::waiting_command = false;
 }
 
-int CommandMonitor::get_decimal_opcode(uint8_t hex_opcode_byte_one, uint8_t hex_opcode_byte_two){
-    uint16_t c_opcode = hex_opcode_byte_one | (hex_opcode_byte_two << 8);
-    std::stringstream ss_opcode;
-    ss_opcode << c_opcode;
-    int f_opcode;
-    ss_opcode >> f_opcode;
-    return f_opcode;
+uint16_t CommandMonitor::get_decimal_opcode(const uint8_t* hex_opcode_bytes){
+    return (hex_opcode_bytes[1] << 8) | (hex_opcode_bytes[0]);
 }
 
-int CommandMonitor::get_decimal_arg(uint8_t hex_arg_byte_one, uint8_t hex_arg_byte_two, uint8_t hex_arg_byte_three, uint8_t hex_arg_byte_four){
-    uint32_t c_arg = sfr::rockblock::arg_2[0] | (sfr::rockblock::arg_2[1] << 8) | (sfr::rockblock::arg_2[2] << 16) | (sfr::rockblock::arg_2[3] << 24);
-    std::stringstream ss_arg;
-    ss_arg << c_arg;
-    int f_arg;
-    ss_arg >> f_arg;
-    return f_arg;
+uint32_t CommandMonitor::get_decimal_arg(const uint8_t* hex_arg_bytes){
+    return (hex_arg_bytes[3] << 24) | (hex_arg_bytes[2]) << 16 | (hex_arg_bytes[1] << 8) | (hex_arg_bytes[0]);
 }
-
-
--- src/Monitors/CommandMonitor.hpp
+++ src/Monitors/CommandMonitor.hpp
@@ -8,14 +8,14 @@ class CommandMonitor : public TimedControlTask<void>{
         CommandMonitor(unsigned int offset);
         void execute();
         void dispatch_change_mission_mode();
-        void dispatch_change_true_false(bool value);
+        void dispatch_change_true_false(bool& value);
         void dispatch_change_temperature_mode();
         void dispatch_change_acs_mode();
         void dispatch_change_fault_mode();
         void dispatch_request_image_fragment();
         void dispatch_change_rockblock_downlink_period();
-        int get_decimal_opcode(uint8_t hex_opcode_byte_one, uint8_t hex_opcode_byte_two);
-        int get_decimal_arg(uint8_t hex_arg_byte_one, uint8_t hex_arg_byte_two, uint8_t hex_arg_byte_three, uint8_t hex_arg_byte_four);
+        uint16_t get_decimal_opcode(const uint8_t* hex_opcode_bytes);
+        uint32_t get_decimal_arg(const uint8_t* hex_arg_bytes);
 };
 
 #endif
