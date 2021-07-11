2f0d0f6d46ce48687a87d80594e6d9be1d27ad3d
hwm44 <hwm44@cornell.edu>
2021-07-10 20:53:33 -0400

    additional simulator tests, flush incomplete

--- test/test_simulator/test_simulator.cpp
+++ test/test_simulator/test_simulator.cpp
@@ -178,12 +178,13 @@ void test_downlink_incorrect_checksum() {
     for(size_t i = 0; i < 70; i++) {
         sim.write(0);
     }
+
     // write INCORRECT checksum
     sim.write(0);
     sim.write(1);
     sim.write('\r');
 
-     // grab entire response
+    // grab entire response
     std::string s2;
     while(sim.available()) {
         s2 += (char) sim.read();
@@ -199,14 +200,263 @@ void test_mailbox_empty() {
     RockblockSimulator sim;
     sim.begin(constants::rockblock::baud);
 
+    // send binary message length command
+    sim.print("AT+SBDWB=70\r");
+
+    // grab entire response
+    std::string s1;
+    while(sim.available()) {
+        s1 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r1 = "AT+SBDWB=70\r\r\nREADY\r\n";
+    TEST_ASSERT(s1 == r1);
+
+    // write 70 zeros
+    for(size_t i = 0; i < 70; i++) {
+        sim.write(0);
+    }
+    // write CORRECT checksum
+    sim.write(0);
+    sim.write(0);
+    sim.write('\r');
+
+     // grab entire response
+    std::string s2;
+    while(sim.available()) {
+        s2 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r2 = "\r\n0\r\n\r\nOK\r\n";
+    TEST_ASSERT(s2 == r2);
+
+    // check mailbox
+    sim.print("AT+SBDIX\r");
+
+    // grab entire response
+    std::string s3;
+    while(sim.available()) {
+        s3 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r3 = "AT+SBDIX\r\r\n+SBDIX: 0, 1, 0, 1, 0, 0\r\n\r\nOK\r\n";
+    TEST_ASSERT(s3 == r3);
 }
 
 void test_receive_uplink() {
+    // initialize sim
+    RockblockSimulator sim;
+    sim.begin(constants::rockblock::baud);
 
+    // insert command for downlink
+    sim.insert("0A0B0C0D0E0F01020304");
+
+    // send binary message length command
+    sim.print("AT+SBDWB=70\r");
+
+    // grab entire response
+    std::string s1;
+    while(sim.available()) {
+        s1 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r1 = "AT+SBDWB=70\r\r\nREADY\r\n";
+    TEST_ASSERT(s1 == r1);
+
+    // write 70 zeros
+    for(size_t i = 0; i < 70; i++) {
+        sim.write(0);
+    }
+    // write CORRECT checksum
+    sim.write(0);
+    sim.write(0);
+    sim.write('\r');
+
+     // grab entire response
+    std::string s2;
+    while(sim.available()) {
+        s2 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r2 = "\r\n0\r\n\r\nOK\r\n";
+    TEST_ASSERT(s2 == r2);
+
+    // check mailbox
+    sim.print("AT+SBDIX\r");
+
+    // grab entire response
+    std::string s3;
+    while(sim.available()) {
+        s3 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r3 = "AT+SBDIX\r\r\n+SBDIX: 0, 1, 1, 1, 10, 0\r\n\r\nOK\r\n";
+    TEST_ASSERT(s3 == r3);
+
+    // read binary data
+    sim.print("AT+SBDRB\r");
+
+    // grab entire response
+    std::string s4;
+    while(sim.available()) {
+        s4 += (char) sim.read();
+    }
+
+    // confirm correct response
+    std::string r4;
+    r4 += "AT+SBDRB\r";
+    r4 += (char) 0;
+    r4 += (char) 10;
+    r4 += (char) 10;
+    r4 += (char) 11;
+    r4 += (char) 12;
+    r4 += (char) 13;
+    r4 += (char) 14;
+    r4 += (char) 15;
+    r4 += (char) 1;
+    r4 += (char) 2;
+    r4 += (char) 3;
+    r4 += (char) 4;
+    r4 += (char) 0;
+    r4 += (char) 85;
+    r4 += "\r\nOK\r\n";
+    TEST_ASSERT(s4 == r4);
 }
 
 void test_flush() {
+    // initialize sim
+    RockblockSimulator sim;
+    sim.begin(constants::rockblock::baud);
 
+    // insert commands for downlink
+    sim.insert("0A0B0C0D0E0F01020304");
+    sim.insert("0A0B0C0D0E0F01020304");
+    sim.insert("0A0B0C0D0E0F01020304");
+    sim.insert("0A0B0C0D0E0F01020304");
+
+    for(size_t i = 0; i < 4; i++) {
+        if(i != 2) {
+            // send binary message length command
+            sim.print("AT+SBDWB=70\r");
+
+            // grab entire response
+            std::string s1;
+            while(sim.available()) {
+                s1 += (char) sim.read();
+            }
+
+            // confirm correct response
+            std::string r1 = "AT+SBDWB=70\r\r\nREADY\r\n";
+            TEST_ASSERT(s1 == r1);
+
+            // write 70 zeros
+            for(size_t i = 0; i < 70; i++) {
+                sim.write(0);
+            }
+
+            // write CORRECT checksum
+            sim.write(0);
+            sim.write(0);
+            sim.write('\r');
+
+            // grab entire response
+            std::string s2;
+            while(sim.available()) {
+                s2 += (char) sim.read();
+            }
+
+            // confirm correct response
+            std::string r2 = "\r\n0\r\n\r\nOK\r\n";
+            TEST_ASSERT(s2 == r2);
+        } else {
+            sim.print("AT+SBDWT=FLUSH_MT\r");
+
+             // grab entire response
+            std::string s2;
+            while(sim.available()) {
+                s2 += (char) sim.read();
+            }
+
+            // confirm correct response
+            std::string r2 = "AT+SBDWT=FLUSH_MT\r\r\nOK\r\n";
+            TEST_ASSERT(s2 == r2);
+        }
+
+        // check mailbox
+        sim.print("AT+SBDIX\r");
+
+        // grab entire response
+        std::string s3;
+        while(sim.available()) {
+            s3 += (char) sim.read();
+        }
+
+        // confirm correct response
+        std::string r3;
+        r3 += "AT+SBDIX\r\r\n+SBDIX: ";
+        switch(i) {
+            case 0:
+                r3 += "0, 1, 1, 1, 10, 3";
+                break;
+            case 1:
+                r3 += "0, 2, 1, 2, 10, 2";
+                break;
+            case 2:
+                r3 += "0, 3, 1, 3, 10, 1";
+                break;
+            case 3:
+                r3 += "0, 4, 1, 4, 10, 0";
+                break;
+        }
+        r3 += "\r\n\r\nOK\r\n";
+        TEST_ASSERT(s3 == r3);
+
+        // read binary data
+        sim.print("AT+SBDRB\r");
+
+        // grab entire response
+        std::string s4;
+        while(sim.available()) {
+            s4 += (char) sim.read();
+        }
+
+        // confirm correct response
+        std::string r4;
+        r4 += "AT+SBDRB\r";
+        if(i != 3) {
+            r4 += (char) 0;
+            r4 += (char) 10;
+            r4 += (char) 10;
+            r4 += (char) 11;
+            r4 += (char) 12;
+            r4 += (char) 13;
+            r4 += (char) 14;
+            r4 += (char) 15;
+            r4 += (char) 1;
+            r4 += (char) 2;
+            r4 += (char) 3;
+            r4 += (char) 4;
+            r4 += (char) 0;
+            r4 += (char) 85;
+        } else {
+            r4 += (char) 0;
+            r4 += (char) 10;
+            r4 += "FLUSH_MT\r\n";
+            r4 += (char) 2;
+            r4 += (char) 153;
+            for(char c : s4) {
+                Serial.println(c, DEC);
+            }
+        }
+        r4 += "\r\nOK\r\n";
+        TEST_ASSERT(s4 == r4);
+    }
 }
 
 int test_simulator() {
@@ -218,8 +468,9 @@ int test_simulator() {
     RUN_TEST(test_flow_control);
     RUN_TEST(test_downlink_correct_checksum);
     RUN_TEST(test_downlink_incorrect_checksum);
-    // RUN_TEST(test_mailbox_empty);
-    // RUN_TEST(test_flush);
+    RUN_TEST(test_mailbox_empty);
+    RUN_TEST(test_receive_uplink);
+    // RUN_TEST(test_flush); // not functional
     return UNITY_END();
 }
 
