9531d788f42428b4b61e1adebc134ca69941967b
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-07-21 20:11:46 -0400

    added comments

--- src/Monitors/CameraReportMonitor.cpp
+++ src/Monitors/CameraReportMonitor.cpp
@@ -33,6 +33,7 @@ void CameraReportMonitor::execute(){
 }
 
 void CameraReportMonitor::create_camera_report(int fragment_number, short serial_number){
+    //open image file and read it for specified image/fragment
     String filename = "";
     if (serial_number < 10){
         filename += "0";
@@ -43,6 +44,8 @@ void CameraReportMonitor::create_camera_report(int fragment_number, short serial
     }
     filename += String(fragment_number) + ".jpg";
     imgFile = SD.open(filename.c_str(), FILE_READ);
+
+    //parse hex stored as chars into actual hex
     uint8_t tempbuffer[constants::camera::content_length*2];
     uint8_t parsedbuffer[constants::camera::content_length];
     for(int i = 0; i < sizeof(tempbuffer); i++){
@@ -58,24 +61,28 @@ void CameraReportMonitor::create_camera_report(int fragment_number, short serial
         } else{
             byte_0 = tempbuffer[i]- 48;
         }
-
         if(tempbuffer[i+1] <= 90 && tempbuffer[i+1] >= 65){
             byte_1 = tempbuffer[i+1]- 55;
         } else{
             byte_1 = tempbuffer[i+1]- 48;
         }
-
         parsedbuffer[x] = byte_1 + (byte_0 * 16);
         x++;
         i++;
     }
     imgFile.close();
+
+    //get each byte of serial number and add to camera report
     sfr::rockblock::camera_report[0] = (serial_number & 255);
     sfr::rockblock::camera_report[1] = ((serial_number >> 8) & 255);
+
+    //get each byte of fragment number
     std::vector<unsigned char> fragment(constants::camera::bytes_allocated_fragment);
     for (size_t i = 0; i < constants::camera::bytes_allocated_fragment; i++){
         fragment[3 - i] = (fragment_number >> (i * 8));
     }
+
+    //add fragment number to camera report
     size_t i = 0;
     int a = constants::camera::bytes_allocated_serial;
     while(i < constants::camera::bytes_allocated_fragment){
@@ -83,6 +90,8 @@ void CameraReportMonitor::create_camera_report(int fragment_number, short serial
         a = a + 1;
         i = i + 1;
     }
+
+    //add actual image content to camera report
     int z = 0;
     int y = constants::camera::bytes_allocated_serial + constants::camera::bytes_allocated_fragment;
     while(z < constants::camera::content_length){
@@ -95,11 +104,15 @@ void CameraReportMonitor::create_camera_report(int fragment_number, short serial
 }
 
 void CameraReportMonitor::add_possible_command(){
+    //convert endianness
     uint32_t converted_serial =  __builtin_bswap32(sfr::camera::current_serial);
+
+    //add opcode to possible commands
     for(int i = 0; i < constants::rockblock::opcode_len; i++){
         sfr::rockblock::camera_commands[sfr::camera::current_serial][i] = constants::rockblock::request_image_fragment[i];
     }
 
+    //add argument 1 to possible commands
     for(int i = constants::rockblock::opcode_len; i < (constants::rockblock::arg1_len+constants::rockblock::opcode_len); i++){
         sfr::rockblock::camera_commands[sfr::camera::current_serial][i] = (converted_serial >> (8*i)) & 0xff;
     }
