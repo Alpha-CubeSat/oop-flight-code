6e6216ba40df3687d02090f90b0bea420d6acb40
Eric Zhang <ewz4@cornell.edu>
2021-10-20 17:20:49 -0700

    replaced all digitalWrite calls with setPinState

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@ void setup(){
     //delay(7200000);
     //pin_control_task.execute();  
     pinMode(14,OUTPUT);
-    digitalWrite(14,LOW);
+    Pins::setPinState(14,LOW);
     delay(2000);
-    digitalWrite(14,HIGH);
+    Pins::setPinState(14,HIGH);
     delay(500);
-    digitalWrite(14,LOW);
+    Pins::setPinState(14,LOW);
 }
 
 void loop(){
