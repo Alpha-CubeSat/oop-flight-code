8548fb29c6962e3850f0b2234794eac1646c4e21
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-10-24 16:40:59 -0400

    modified acs control task

--- src/Control Tasks/ACSControlTask.cpp
+++ src/Control Tasks/ACSControlTask.cpp
@@ -5,46 +5,44 @@ ACSControlTask::ACSControlTask(unsigned int offset) : TimedControlTask<void>(off
 
 void ACSControlTask::execute()
 {
-    /*ACSWrite(constants::acs::xtorqorder, sfr::acs::current1, constants::acs::xout1, constants::acs::xout2, constants::acs::xPWMpin);
+    ACSWrite(constants::acs::xtorqorder, sfr::acs::current1, constants::acs::xout1, constants::acs::xout2, constants::acs::xPWMpin);
     ACSWrite(constants::acs::ytorqorder, sfr::acs::current2, constants::acs::yout1, constants::acs::yout2, constants::acs::yPWMpin);
-    ACSWrite(constants::acs::ztorqorder, sfr::acs::current3, constants::acs::zout1, constants::acs::zout2, constants::acs::zPWMpin);*/
+    ACSWrite(constants::acs::ztorqorder, sfr::acs::current3, constants::acs::zout1, constants::acs::zout2, constants::acs::zPWMpin);
 }
 
-void ACSControlTask::ACSWrite(int torqorder, int current, int out1, int out2, int PWMpin)
-{
-    if (current == 0.0)
-    {
-        Pins::setPinState(out1, LOW);
-        Pins::setPinState(out2, LOW);
+int ACSControlTask::current2PWM(float current) {
+  // returns PWM value
+}
+
+void ACSControlTask::ACSWrite(int torqorder,  float current,  int out1,  int out2, int PWMpin) {
+    int PWM = current2PWM(abs(current));
+    
+    if (PWM == 0) {
+        digitalWrite(out1, LOW);
+        digitalWrite(out2, LOW);
     }
-    if (torqorder == 0)
-    {
-        if (current > 0)
-        {
-            Pins::setPinState(out1, HIGH);
-            Pins::setPinState(out2, LOW);
-            analogWrite(PWMpin, current);
+    else if (torqorder == 0) { 
+        if (current > 0) {
+            digitalWrite(out1, HIGH);
+            digitalWrite(out2, LOW);
+            analogWrite(PWMpin, PWM);
         }
-        if (current < 0)
-        {
-            Pins::setPinState(out1, LOW);
-            Pins::setPinState(out2, HIGH);
-            analogWrite(PWMpin, current);
+        if (current < 0) {
+            digitalWrite(out1, LOW);
+            digitalWrite(out2, HIGH);
+            analogWrite(PWMpin, PWM);
         }
     }
-    if (torqorder == 1)
-    {
-        if (current > 0)
-        {
-            Pins::setPinState(out1, LOW);
-            Pins::setPinState(out2, HIGH);
-            analogWrite(PWMpin, current);
+    else if (torqorder == 1) {
+        if (current > 0) {
+            digitalWrite(out1, LOW);
+            digitalWrite(out2, HIGH);
+            analogWrite(PWMpin, PWM);
         }
-        if (current < 0)
-        {
-            Pins::setPinState(out1, HIGH);
-            Pins::setPinState(out2, LOW);
-            analogWrite(PWMpin, current);
+        if (current < 0) {
+            digitalWrite(out1, HIGH);
+            digitalWrite(out2, LOW);
+            analogWrite(PWMpin, PWM);
         }
     }
 }
