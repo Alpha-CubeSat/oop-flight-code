a476a7f793975eccd674f2f7ae209e756c12b838
Eric <ewz4@cornell.edu>
2021-11-18 14:29:11 -0500

    asdf

--- src/ResetSFR.hpp
+++ src/ResetSFR.hpp
@@ -9,6 +9,8 @@ public:
   static void capture_default_sfr();
   static void restore_default_sfr();
 private:
+// need a pinMap reset? If yes, we can just call PinControlTask::execute().
+// pins are being updated in the switch cases in each control task anyway
   static bool photoresistor_covered;
 
   static mission_mode_type mission_mode;
@@ -56,6 +58,8 @@ private:
   static uint8_t rockblock_opcode[2];
   static uint8_t rockblock_arg_1[4];
   static uint8_t rockblock_arg_2[4];
+  static HardwareSerial serial;
+  static RockblockSimulator serial;
   static bool rockblock_flush_status;
   static bool rockblock_waiting_command;
   static size_t rockblock_conseq_reads;
@@ -66,6 +70,61 @@ private:
   static int rockblock_start_time;
   static bool rockblock_last_timed_out;
   static int rockblock_num_downlinks;
+
+  static sensor_mode_type imu_mode;
+  static int imu_max_retry_attempts;
+  static float imu_mag_x;
+  static float imu_mag_y;
+  static float imu_mag_z;
+  static float imu_gyro_x;
+  static float imu_gyro_y;
+  static float imu_gyro_z;
+  static std::deque<float> imu_mag_x_buffer;
+  static std::deque<float> imu_mag_y_buffer;
+  static std::deque<float> imu_mag_z_buffer;
+  static std::deque<float> imu_gyro_x_buffer;
+  static std::deque<float> imu_gyro_y_buffer;
+  static std::deque<float> imu_gyro_z_buffer;
+  static float imu_mag_x_average;
+  static float imu_mag_y_average;
+  static float imu_mag_z_average;
+  static float gyro_x_average;
+  static float gyro_y_average;
+  static float gyro_z_average;
+
+  static float temperature_temp_c;
+  static std::deque<float> temperature_temp_c_buffer;
+  static float temperature_temp_c_average;
+  static temp_mode_type temperature_mode;
+  static bool temperature_in_sun;
+
+  static float current_solar_current;
+  static std::deque<float> current_solar_current_buffer;
+  static float current_solar_current_average;
+  static bool current_in_sun;
+
+  static acs_mode_type acs_mode;
+  static float acs_current1;
+  static float acs_current2;
+  static float acs_current3;
+  static simple_acs_type acs_mag;
+  static unsigned long acs_max_no_communication;
+
+  static float battery_voltage;
+  static std::deque<float> battery_voltage_buffer;
+  static float battery_voltage_average;
+
+  static fault_mode_type fault_mode;
+  static unsigned char fault_fault_1;
+  static unsigned char fault_fault_2;
+  static unsigned char fault_fault_3;
+
+
+
+
+
+
+
 
 
 
