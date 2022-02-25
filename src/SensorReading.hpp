590b90a81f78b4463cd486417a98c1763da70874
Weiyan Wu <ww333@cornell.edu>
2022-02-25 00:10:38 -0500

    changed fault type

--- src/SensorReading.hpp
+++ src/SensorReading.hpp
@@ -10,12 +10,12 @@ private:
     bool fault_status; // Fault flag: true means valid reading and false means invalid reading
 
 public:
-    SensorReading(float x, unsigned char fault); // Constructor
-    float get_value();                           // Get the sensor reading
-    bool is_valid();                             // Get the fault status
-    void set_value(float value);                 // Set the value of a SensorReading object
-    void set_valid();                            // Set the fault flag of a SensorReading object to 1
-    void set_invalid();                          // Set the fault flag of a SensorReading object to 0
+    SensorReading(float x, bool fault); // Constructor
+    float get_value();                  // Get the sensor reading
+    bool is_valid();                    // Get the fault status
+    void set_value(float value);        // Set the value of a SensorReading object
+    void set_valid();                   // Set the fault flag of a SensorReading object to 1
+    void set_invalid();                 // Set the fault flag of a SensorReading object to 0
 };
 
 #endif
