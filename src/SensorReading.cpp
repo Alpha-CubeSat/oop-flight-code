bbb8f37ebce363293ca994505883219a6d2be065
Weiyan Wu <ww333@cornell.edu>
2022-02-24 23:56:03 -0500

    add SensorReading class

---
+++ src/SensorReading.cpp
@@ -0,0 +1,32 @@
+#include "SensorReading.hpp"
+
+SensorReading::SensorReading(float x, unsigned char fault)
+{
+    value = x;
+    fault_status = fault;
+}
+
+float SensorReading::get_value()
+{
+    return value;
+}
+
+bool SensorReading::is_valid()
+{
+    return fault_status;
+}
+
+void SensorReading::set_value(float x)
+{
+    this->value = x;
+}
+
+void SensorReading::set_valid()
+{
+    this->fault_status = false;
+}
+
+void SensorReading::set_invalid()
+{
+    this->fault_status = true;
+}
