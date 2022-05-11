6307d3f43566add0dc2f1a393d2683650f9da888
ljgreenhill <ljgreenhill3@gmail.com>
2022-05-10 23:23:05 -0400

    removed duplicate boot initialization

--- src/MainControlLoop.cpp
+++ src/MainControlLoop.cpp
@@ -4,7 +4,6 @@ MainControlLoop::MainControlLoop()
     : ControlTask<void>(), clock_manager(constants::timecontrol::control_cycle_time), acs_monitor(constants::timecontrol::acs_monitor_offset), battery_monitor(constants::timecontrol::battery_monitor_offset), button_monitor(constants::timecontrol::button_monitor_offset), camera_report_monitor(constants::timecontrol::camera_report_monitor_offset), command_monitor(constants::timecontrol::command_monitor_offset), current_monitor(constants::timecontrol::current_monitor_offset), fault_monitor(constants::timecontrol::fault_monitor_offset), imu_monitor(constants::timecontrol::imu_monitor_offset), normal_report_monitor(constants::timecontrol::normal_report_monitor_offset), photoresistor_monitor(constants::timecontrol::photoresistor_monitor_offset), rockblock_report_monitor(constants::timecontrol::rockblock_report_monitor_offset), temperature_monitor(constants::timecontrol::temperature_monitor_offset), acs_control_task(constants::timecontrol::acs_control_task_offset), burnwire_control_task(constants::timecontrol::burnwire_control_task_offset), camera_control_task(constants::timecontrol::camera_control_task_offset), rockblock_control_task(constants::timecontrol::rockblock_control_task_offset), mission_manager(constants::timecontrol::mission_manager_offset)
 {
     delay(1000);
-    boot_initialization();
 }
 
 void MainControlLoop::execute()
