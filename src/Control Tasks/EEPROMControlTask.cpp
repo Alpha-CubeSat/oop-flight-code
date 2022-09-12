c8402cd0e04511092562bacfcbd3178c372ff3b0
Eric <ewz4@cornell.edu>
2022-09-11 20:19:45 -0400

    Addressed PR requested changes

--- src/Control Tasks/EEPROMControlTask.cpp
+++ src/Control Tasks/EEPROMControlTask.cpp
@@ -7,28 +7,34 @@ EEPROMControlTask::EEPROMControlTask(unsigned int offset)
 
 void EEPROMControlTask::execute()
 {
+
+    // WAIT TIME TRACKING
     if (!sfr::EEPROM::alloted_time_passed) {
-        // The wait time of two hours has not been reached since last read
-        // the amount of time that the EEPROM has counted, stops when the alloted time has been reached
+        /* The wait time of two hours was not reached at the previous EEPROM read. Since then, the wait time
+        has increased and re-written to EEPROM, and will be read again in the next lines. */
         int byte0 = EEPROM.read(0) << 24;
         int byte1 = EEPROM.read(1) << 16;
         int byte2 = EEPROM.read(2) << 8;
         int byte3 = EEPROM.read(3);
         int eeprom_value = byte0 + byte1 + byte2 + byte3;
-
-        Serial.println("EEPROM Value: " + eeprom_value);
+#ifdef VERBOSE
+        Serial.println("Current EEPROM value: " + String(eeprom_value));
+#endif
         if (eeprom_value > sfr::EEPROM::alloted_time) {
             // The newly read eeprom value exceeds the two hour wait time
             sfr::EEPROM::alloted_time_passed = true;
-            Serial.println("EEPROM Time tracking finished");
+#ifdef VERBOSE
+            Serial.println("EEPROM time tracking finished!");
+#endif
         } else {
             int time_since_last_write = millis() - sfr::EEPROM::time_of_last_write;
 
             if (time_since_last_write > sfr::EEPROM::write_step_time) {
                 // The last EEPROM write exceeds the interval between writes, so update the EEPROM value
                 int write = time_since_last_write + eeprom_value;
-                Serial.println("New value to write: " + write);
-
+#ifdef VERBOSE
+                Serial.println("New value to write: " + String(write));
+#endif
                 // Saves the value in write by bytes
                 EEPROM.write(3, write & 0xFF);
                 EEPROM.write(2, (write >> 8) & 0xFF);
