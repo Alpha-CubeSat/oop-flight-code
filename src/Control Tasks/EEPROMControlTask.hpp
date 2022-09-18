2142425cbd9b0680f065fb627e271d68a214c028
ljgreenhill <ljgreenhill3@gmail.com>
2022-09-18 17:35:52 -0400

    Formatting

--- src/Control Tasks/EEPROMControlTask.hpp
+++ src/Control Tasks/EEPROMControlTask.hpp
@@ -1,9 +1,9 @@
 #ifndef EEPROM_CONTROL_TASK_HPP_
 #define EEPROM_CONTROL_TASK_HPP_
-#include "WString.h"
-#include <EEPROM.h>
-#include "sfr.hpp"
 #include "Control Tasks/TimedControlTask.hpp"
+#include "WString.h"
+#include "sfr.hpp"
+#include <EEPROM.h>
 
 class EEPROMControlTask : public TimedControlTask<void>
 {
