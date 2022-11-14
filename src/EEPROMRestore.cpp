aa6dd102475bf788df826226d53ba8d547af429c
Eric <ewz4@cornell.edu>
2022-11-13 19:40:40 -0500

    final revision

--- src/EEPROMRestore.cpp
+++ src/EEPROMRestore.cpp
@@ -2,26 +2,18 @@
 
 void EEPROMRestore::execute()
 {
-    uint16_t read_addr;
-    EEPROM.get(4, read_addr);
-    sfr::eeprom::sfr_address = read_addr;
+    uint16_t sfr_address;
+    EEPROM.get(4, sfr_address);
+    sfr::eeprom::sfr_address = sfr_address;
 
     for (SFRInterface *s : sfr::sfr_fields_vector) {
-        read_addr = s->getAddressOffset();
+        int read_address = sfr_address + s->getAddressOffset();
         bool restore;
-        EEPROM.get(read_addr, restore);
+        EEPROM.get(read_address, restore);
         if (restore) {
             uint32_t value;
-            EEPROM.get(read_addr + 2, value);
+            EEPROM.get(read_address + 2, value);
             s->setValue(value);
         }
     }
 }
-
-// new plan:
-
-// sfr iterable, dynamically sized data structure of pointers to sfr objects
-// using pointer, see object memory location in EEPROM
-// update object fields from EEPROM
-// write all, but control which ones to restore using presence in data structure
-// move entire section when life cycle limit reached
--- src/SFRField.hpp
+++ src/SFRField.hpp
@@ -13,7 +13,7 @@ private:
     int field_value;    // The value of the field cast to an int
     int data_type;      // An int representing the field's type T, 1 for bool, 2 for uint8_t, 3 for uint16_t, and 4 for uint64_t
     int address_offset; // This field's byte offset from the beginning of the EEPROM section where SFR data is currently stored
-                        // read_address + address_offset gives this field's location in EEPROM
+                        // sfr_address + address_offset gives this field's location in EEPROM
     bool restore;       // If the field should be restored or not
 
 public:
