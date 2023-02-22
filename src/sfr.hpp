ef803d5855dd8403d0faca871989263fefa2b1da
FlatSat Test Bed <lqezczzolxqdugnhst@bvhrs.com>
2023-02-22 13:54:25 -0500

    Added collection_timer setting and conditions to turn IMU on

--- src/sfr.hpp
+++ src/sfr.hpp
@@ -267,7 +267,7 @@ namespace sfr {
         extern SensorReading *gyro_x_value;
         extern SensorReading *gyro_y_value;
         extern SensorReading *gyro_z_value;
-        extern SensorReading *collection_time;
+        extern int collection_time;
 
         extern SensorReading *mag_x_average;
         extern SensorReading *mag_y_average;
