7d114fbfcc1952fb91adbe12cfc32676abec60c1
Duncan-McD <dam342@cornell.edu>
2023-03-26 21:26:33 -0400

    fix formatting on main

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ void setup()
     Serial.begin(9600);
     delay(5000);
     Pins::setInitialPinStates();
-    //TODO: Uncomment when EEPROMRestore is fixed (FS-186)
+    // TODO: Uncomment when EEPROMRestore is fixed (FS-186)
     // EEPROMRestore::execute();
 }
 
