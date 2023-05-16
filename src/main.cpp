6ef8a3beca4b764f407f8e83bfccc699a828ac85
Duncan McDonald <dam342@cornell.edu>
2023-05-16 10:53:51 -0400

    uncommented the eepromRestore hotfix from a while ago (False Alarm - run script Eric Made to fix issue on a teensy by resetting eeprom values) (#117)

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,7 @@ void setup()
     Serial.begin(9600);
     delay(5000);
     Pins::setInitialPinStates();
-    // TODO: Uncomment when EEPROMRestore is fixed (FS-186)
-    // EEPROMRestore::execute();
+    EEPROMRestore::execute();
 }
 
 void loop()
