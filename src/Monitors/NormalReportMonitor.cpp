b1a5c2e18e7169f553af956b149a91379a392e74
Jonathan Ma <jonathanjma03@gmail.com>
2023-09-17 13:56:09 -0400

    add acs indicies to normal report

--- src/Monitors/NormalReportMonitor.cpp
+++ src/Monitors/NormalReportMonitor.cpp
@@ -54,52 +54,56 @@ void NormalReportMonitor::execute()
         sfr::rockblock::waiting_message, sfr::rockblock::waiting_command,
         sfr::temperature::in_sun, sfr::current::in_sun, sfr::button::pressed};
 
-    std::map<uint8_t, uint8_t> report_contents = {
-        {0, 99},
+    std::vector<uint8_t> report_contents{
+        99,
         // SFR fields
-        {1, serialize(sfr::burnwire::burn_time, 0, 5000)},
-        {2, serialize(sfr::burnwire::armed_time, 0, 864000000)},
-        {3, serialize(sfr::rockblock::downlink_period, 60000, 172800000)},
-        {4, (uint8_t)sfr::eeprom::boot_counter},
-        {5, serialize(sfr_packed_bools)},
+        serialize(sfr::burnwire::burn_time, 0, 5000),
+        serialize(sfr::burnwire::armed_time, 0, 864000000),
+        serialize(sfr::rockblock::downlink_period, 60000, 172800000),
+        sfr::eeprom::boot_counter,
+        serialize(sfr::acs::Id_index, 0, 1),
+        serialize(sfr::acs::Kd_index, 0, 1),
+        serialize(sfr::acs::Kp_index, 0, 1),
+        serialize(sfr::acs::c_index, 0, 1),
+        serialize(sfr_packed_bools),
 
         // Sensor readings
-        {6, serialize(light_avg_standby, 0, 1000)},
-        {7, serialize(light_avg_deploy, 0, 1000)},
-        {8, serialize(mag_x, -100, 100)},
-        {9, serialize(mag_y, -100, 100)},
-        {10, serialize(mag_z, -100, 100)},
-        {11, serialize(gyro_x, -100, 100)},
-        {12, serialize(gyro_y, -100, 100)},
-        {13, serialize(gyro_z, -100, 100)},
-        {14, serialize(mag_x_avg, -100, 100)},
-        {15, serialize(mag_y_avg, -100, 100)},
-        {16, serialize(mag_z_avg, -100, 100)},
-        {17, serialize(gyro_x_avg, -100, 100)},
-        {18, serialize(gyro_y_avg, -100, 100)},
-        {19, serialize(gyro_z_avg, -100, 100)},
-        {20, serialize(temp, -500, 500)},
-        {21, serialize(temp_avg, -100, 200)},
-        {22, serialize(current_avg, -75, 500)},
-        {23, serialize(voltage, 0, 5.5)},
-        {24, serialize(voltage_avg, 0, 5.5)},
+        serialize(light_avg_standby, 0, 1000),
+        serialize(light_avg_deploy, 0, 1000),
+        serialize(mag_x, -100, 100),
+        serialize(mag_y, -100, 100),
+        serialize(mag_z, -100, 100),
+        serialize(gyro_x, -100, 100),
+        serialize(gyro_y, -100, 100),
+        serialize(gyro_z, -100, 100),
+        serialize(mag_x_avg, -100, 100),
+        serialize(mag_y_avg, -100, 100),
+        serialize(mag_z_avg, -100, 100),
+        serialize(gyro_x_avg, -100, 100),
+        serialize(gyro_y_avg, -100, 100),
+        serialize(gyro_z_avg, -100, 100),
+        serialize(temp, -500, 500),
+        serialize(temp_avg, -100, 200),
+        serialize(current_avg, -75, 500),
+        serialize(voltage, 0, 5.5),
+        serialize(voltage_avg, 0, 5.5),
 
         // Faults
-        {25, (fault_groups::imu_faults::mag_x_value->serialize() << 4) + fault_groups::imu_faults::mag_x_average->serialize()},
-        {26, (fault_groups::imu_faults::mag_y_value->serialize() << 4) + fault_groups::imu_faults::mag_y_average->serialize()},
-        {27, (fault_groups::imu_faults::mag_z_value->serialize() << 4) + fault_groups::imu_faults::mag_z_average->serialize()},
-        {28, (fault_groups::imu_faults::gyro_x_value->serialize() << 4) + fault_groups::imu_faults::gyro_x_average->serialize()},
-        {29, (fault_groups::imu_faults::gyro_y_value->serialize() << 4) + fault_groups::imu_faults::gyro_y_average->serialize()},
-        {30, (fault_groups::imu_faults::gyro_z_value->serialize() << 4) + fault_groups::imu_faults::gyro_z_average->serialize()},
-        {31, (fault_groups::power_faults::temp_c_value->serialize() << 4) + fault_groups::power_faults::temp_c_average->serialize()},
-        {32, (fault_groups::power_faults::voltage_value->serialize() << 4) + fault_groups::power_faults::voltage_average->serialize()},
-        {33, (fault_groups::power_faults::solar_current_average->serialize() << 4)},
-        {34, (fault_groups::hardware_faults::light_val->serialize() << 4) + fault_groups::hardware_faults::button->serialize()}};
+        (uint8_t)((fault_groups::imu_faults::mag_x_value->serialize() << 4) + fault_groups::imu_faults::mag_x_average->serialize()),
+        (uint8_t)((fault_groups::imu_faults::mag_y_value->serialize() << 4) + fault_groups::imu_faults::mag_y_average->serialize()),
+        (uint8_t)((fault_groups::imu_faults::mag_z_value->serialize() << 4) + fault_groups::imu_faults::mag_z_average->serialize()),
+        (uint8_t)((fault_groups::imu_faults::gyro_x_value->serialize() << 4) + fault_groups::imu_faults::gyro_x_average->serialize()),
+        (uint8_t)((fault_groups::imu_faults::gyro_y_value->serialize() << 4) + fault_groups::imu_faults::gyro_y_average->serialize()),
+        (uint8_t)((fault_groups::imu_faults::gyro_z_value->serialize() << 4) + fault_groups::imu_faults::gyro_z_average->serialize()),
+        (uint8_t)((fault_groups::power_faults::temp_c_value->serialize() << 4) + fault_groups::power_faults::temp_c_average->serialize()),
+        (uint8_t)((fault_groups::power_faults::voltage_value->serialize() << 4) + fault_groups::power_faults::voltage_average->serialize()),
+        (uint8_t)((fault_groups::power_faults::solar_current_average->serialize() << 4)),
+        (uint8_t)((fault_groups::hardware_faults::light_val->serialize() << 4) + fault_groups::hardware_faults::button->serialize())};
 
     std::deque<uint8_t> empty_normal_report;
     std::swap(sfr::rockblock::normal_report, empty_normal_report);
 
-    for (int i = 0; i < report_contents.size(); i++) {
+    for (size_t i = 0; i < report_contents.size(); i++) {
         sfr::rockblock::normal_report.push_back(report_contents[i]);
     }
 
@@ -107,7 +111,7 @@ void NormalReportMonitor::execute()
     // pack mission mode hist into 10 bytes
 
     // push the most recent 20 mission modes switches, starting with most recent
-    int k = 0;
+    size_t k = 0;
     auto hist_mode = sfr::mission::mode_history.cbegin();
     auto prev_hist_mode = sfr::mission::mode_history.cbegin();
     while (hist_mode != sfr::mission::mode_history.cend() && k <= sfr::mission::mission_mode_hist_length) {
@@ -124,7 +128,7 @@ void NormalReportMonitor::execute()
     // add delimeter between mission mode history and processed opcodes
     sfr::rockblock::normal_report.push_back(constants::rockblock::normal_report_delimiter);
 
-    int j = 0;
+    size_t j = 0;
     auto current_command = sfr::rockblock::commands_received.cbegin();
     while (current_command != sfr::rockblock::commands_received.cend() && j < sfr::rockblock::normal_report_command_max) {
         sfr::rockblock::normal_report.push_back(*current_command);
