f307c956ba03e2b111b3f6128a6835f6c107510f
Lauren Greenhill <ljgreenhill3@gmail.com>
2023-09-29 21:27:09 -0400

    Update photoresistor variables (#140)

--- src/Monitors/PhotoresistorMonitor.cpp
+++ src/Monitors/PhotoresistorMonitor.cpp
@@ -13,28 +13,19 @@ void PhotoresistorMonitor::execute()
         initialized = true;
     }
 
-    float val = analogRead(constants::photoresistor::pin);
-    bool possible_uncovered = false;
-
+    val = analogRead(constants::photoresistor::pin);
     sfr::photoresistor::light_val_average_standby->set_value(val);
     sfr::photoresistor::light_val_average_deployment->set_value(val);
-    bool avg_buffer_valid = sfr::photoresistor::light_val_average_standby->get_value(&val);
-    bool deploy_buffer_valid = sfr::photoresistor::light_val_average_deployment->get_value(&val) && val > constants::photoresistor::light_val;
+    avg_buffer_valid = sfr::photoresistor::light_val_average_standby->get_value(&val);
+    deploy_buffer_valid = sfr::photoresistor::light_val_average_deployment->get_value(&val) && val > constants::photoresistor::light_val;
 
     // if sun detected, set photoresistor to uncovered (regardless of validity)
     if (val > constants::photoresistor::light_val) {
         sfr::photoresistor::covered = false;
         // if either buffer is valid, check whether to invalidate
         if (avg_buffer_valid || deploy_buffer_valid) {
-            for (int mission_mode : sfr::mission::mode_history) {
-                // checks if mission mode has never reached burn
-                if (mission_mode >= sfr::mission::mandatoryBurns->get_id()) {
-                    possible_uncovered = true;
-                }
-            }
-
             // set buffer to invalid if never reached burn
-            if (!possible_uncovered) {
+            if (!sfr::mission::possible_uncovered) {
                 if (sfr::photoresistor::light_val_average_standby->get_value(&val)) {
                     sfr::photoresistor::light_val_average_standby->set_invalid();
                 } else {
--- src/Monitors/PhotoresistorMonitor.hpp
+++ src/Monitors/PhotoresistorMonitor.hpp
@@ -14,6 +14,9 @@ public:
 
 private:
     bool initialized = false;
+    bool avg_buffer_valid;
+    bool deploy_buffer_valid;
+    float val;
 };
 
 #endif
