ca100e177d728fcb3a2d1f1cb56bf0b5fbe2d1a7
Lauren Greenhill <ljgreenhill3@gmail.com>
2023-10-02 12:06:43 -0400

    Initialize rockblock in standby

--- src/sfr.cpp
+++ src/sfr.cpp
@@ -156,7 +156,7 @@ namespace sfr {
         SFRField<bool> sleep_mode = SFRField<bool>(false, 0x2103);
         SFRField<uint8_t> max_commands_count = SFRField<uint8_t>(10, 0x2104);
         SFRField<uint16_t> downlink_report_type = SFRField<uint16_t>((uint16_t)report_type::normal_report, 0x2105);
-        SFRField<uint16_t> mode = SFRField<uint16_t>((uint16_t)rockblock_mode_type::send_at, 0x2106);
+        SFRField<uint16_t> mode = SFRField<uint16_t>((uint16_t)rockblock_mode_type::standby, 0x2106);
         SFRField<uint32_t> last_downlink = SFRField<uint32_t>(0, 0x2107);
         // TODO actual default values
         SFRField<uint32_t> downlink_period = SFRField<uint32_t>(20 * constants::time::one_minute, constants::time::one_second, 2 * constants::time::one_day, 0x2108);
