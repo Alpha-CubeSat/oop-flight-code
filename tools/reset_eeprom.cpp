586967abe5b5743f8cf0f582d67b83c2b2a3126c
Cameron Goddard <camerongoddard@me.com>
2023-10-14 14:25:33 -0400

    merge sd card clearing into reset eeprom script

--- tools/reset_eeprom.cpp
+++ tools/reset_eeprom.cpp
@@ -1,12 +1,60 @@
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <SD.h>
+#include <string>
 
-// Script for resetting all EERPOM bytes
+#define NUM_FILES 10
+
+bool create_files = false;
+
+// Script for resetting all EERPOM bytes and clearing the SD card
 // Set PlatformIO environment to `env:reset_eeprom` then upload.
 
 void setup()
 {
     Serial.begin(9600);
+
+    if (!SD.begin(254)) {
+        Serial.println("Error: Failed to init SD card");
+        return;
+    }
+
+    // Create files if specified
+    if (create_files) {
+
+        for (int i = 0; i < NUM_FILES; i++) {
+            std::string filename = "file" + std::to_string(i) + ".txt";
+            File file = SD.open(filename.c_str(), FILE_WRITE);
+            Serial.print("Creating ");
+            Serial.println(filename.c_str());
+            file.close();
+        }
+    }
+
+    // Delete files
+    File root = SD.open("/");
+
+    if (root) {
+        while (true) {
+            File entry = root.openNextFile();
+            Serial.print("Deleting ");
+            Serial.println(entry.name());
+
+            if (!entry) {
+                break;
+            }
+
+            if (!SD.remove(entry.name())) {
+                Serial.print("Deleting ");
+                Serial.print(entry.name());
+                Serial.println(" unsuccessful");
+            }
+        }
+        root.close();
+    } else {
+        Serial.println("Error: Opening root failed");
+    }
+    Serial.println("All files deleted");
 }
 
 void loop()
