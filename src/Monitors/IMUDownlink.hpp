2f770b5f3b747f8d6424a1052c9d38b1fec03a14
Eric Zhang <70923541+ewz4@users.noreply.github.com>
2024-01-04 22:34:09 -0500

    Added comments, removed bad camera constant (#171)

--- src/Monitors/IMUDownlink.hpp
+++ src/Monitors/IMUDownlink.hpp
@@ -14,8 +14,8 @@ public:
 private:
     Adafruit_LSM9DS1 imu;
     uint8_t fragment_number = 0;
-    uint32_t values_unwritten;
-    uint32_t start_index = 0;
+    uint32_t values_unwritten; // The number of IMU downlink values not yet written to the SD card
+    uint32_t start_index = 0;  // The index in sfr::imu::imu_dlink of where the IMU downlink values not written to the SD card start
 };
 
 #endif
