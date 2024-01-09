6886059bb199db409ef8017b1264fd2e146bfb0b
Lauren Greenhill <ljgreenhill3@gmail.com>
2024-01-09 10:10:34 -0500

    Fis boot time mins

--- src/ClockManager.cpp
+++ src/ClockManager.cpp
@@ -7,7 +7,7 @@ ClockManager::ClockManager()
 void ClockManager::execute()
 {
     sfr::mission::cycle_no++;
-    sfr::mission::boot_time_mins = sfr::mission::cycle_no % (constants::time::one_minute / constants::time::control_cycle_time_ms);
+    sfr::mission::boot_time_mins = (sfr::mission::cycle_no * constants::time::control_cycle_time_ms) / constants::time::one_minute;
 
     cycle_time = millis() - sfr::mission::cycle_start;
 
