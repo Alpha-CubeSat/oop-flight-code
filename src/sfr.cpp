0cdea2786fd7a3681b838e83ad3029369611edb3
Lauren Greenhill <ljgreenhill3@gmail.com>
2024-03-24 21:57:26 -0400

    resolution passed in last

--- src/sfr.cpp
+++ src/sfr.cpp
@@ -217,7 +217,7 @@ namespace sfr {
         SFRField<bool> off = SFRField<bool>(true, 0x2500);
         SFRField<uint8_t> mode = SFRField<uint8_t>((uint8_t)acs_mode_type::detumble, 0x2501);
         SFRField<uint8_t> simple_mag = SFRField<uint8_t>((uint8_t)mag_type::z, 0x2502);
-        SFRField<uint8_t> target_spin_rate = SFRField<uint8_t>(5.0 * constants::acs::spin_resolution, constants::acs::spin_resolution, 0x2503);
+        SFRField<uint8_t> target_spin_rate = SFRField<uint8_t>(5.0 * constants::acs::spin_resolution, 0x2503, constants::acs::spin_resolution);
         SFRField<uint32_t> simple_current = SFRField<uint32_t>((0 - constants::acs::current_offset) * constants::acs::current_resolution, (-0.25 - constants::acs::current_offset) * constants::acs::current_resolution, (0.25 - constants::acs::current_offset) * constants::acs::current_resolution, 0x2504, constants::acs::current_resolution, constants::acs::current_offset);
         SFRField<uint32_t> on_time = SFRField<uint32_t>(5 * constants::time::one_minute, 0, constants::time::one_revolution, 0x2505);
         SFRField<uint32_t> Id_index = SFRField<uint32_t>(0, 0, (sizeof(constants::acs::Id_values) / sizeof(constants::acs::Id_values[0])) - 1, 0x2506);
