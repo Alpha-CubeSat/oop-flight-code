6db31e6d6cc06f02b2a3429750ea93aa99dd520e
Lauren Greenhill <ljgreenhill3@gmail.com>
2024-04-20 13:15:38 -0400

    SFR changes for faster "nominal" test

--- src/sfr.cpp
+++ src/sfr.cpp
@@ -4,11 +4,11 @@ namespace sfr {
     namespace stabilization {
         // OP Codes 1100
         // TODO actual default value
-        SFRField<uint32_t> max_time = SFRField<uint32_t>(2 * constants::time::one_hour, 0x1100);
+        SFRField<uint32_t> max_time = SFRField<uint32_t>(5 * constants::time::one_minute, 0x1100);
     } // namespace stabilization
     namespace boot {
         // OP Codes 1200
-        SFRField<uint32_t> max_time = SFRField<uint32_t>(2 * constants::time::one_hour, 0x1200);
+        SFRField<uint32_t> max_time = SFRField<uint32_t>(5 * constants::time::one_minute, 0x1200);
     } // namespace boot
     namespace detumble {
         // OP Codes 1500
@@ -151,8 +151,8 @@ namespace sfr {
         SFRField<uint16_t> downlink_report_type = SFRField<uint16_t>((uint16_t)report_type::normal_report, 0x2106);
         SFRField<uint16_t> mode = SFRField<uint16_t>((uint16_t)rockblock_mode_type::standby, 0x2107);
         SFRField<uint32_t> last_downlink = SFRField<uint32_t>(0, 0x2108);
-        SFRField<uint32_t> downlink_period = SFRField<uint32_t>(20 * constants::time::one_minute, 0, 2 * constants::time::one_day, 0x2109);
-        SFRField<uint32_t> lp_downlink_period = SFRField<uint32_t>(constants::time::one_hour, constants::time::one_second, 2 * constants::time::one_day, 0x2110);
+        SFRField<uint32_t> downlink_period = SFRField<uint32_t>(5 * constants::time::one_minute, 0, 2 * constants::time::one_day, 0x2109);
+        SFRField<uint32_t> lp_downlink_period = SFRField<uint32_t>(5 * constants::time::one_minute, constants::time::one_second, 2 * constants::time::one_day, 0x2110);
         SFRField<uint32_t> transmit_downlink_period = SFRField<uint32_t>(20 * constants::time::one_minute, constants::time::one_second, 2 * constants::time::one_day, 0x2111);
         SFRField<uint32_t> on_time = SFRField<uint32_t>(35 * constants::time::one_minute, 0, constants::time::one_revolution, 0x2112);
 
@@ -219,7 +219,7 @@ namespace sfr {
         SFRField<uint8_t> simple_mag = SFRField<uint8_t>((uint8_t)mag_type::z, 0x2502);
         SFRField<uint8_t> target_spin_rate = SFRField<uint8_t>((5.0 * constants::acs::spin_resolution), 0x2503, constants::acs::spin_resolution);
         SFRField<uint32_t> simple_current = SFRField<uint32_t>((0 - constants::acs::current_offset) * constants::acs::current_resolution, (-0.25 - constants::acs::current_offset) * constants::acs::current_resolution, (0.25 - constants::acs::current_offset) * constants::acs::current_resolution, 0x2504, constants::acs::current_resolution, constants::acs::current_offset);
-        SFRField<uint32_t> on_time = SFRField<uint32_t>(5 * constants::time::one_minute, 0, constants::time::one_revolution, 0x2505);
+        SFRField<uint32_t> on_time = SFRField<uint32_t>(1 * constants::time::one_minute, 0, constants::time::one_revolution, 0x2505);
         SFRField<uint32_t> Id_index = SFRField<uint32_t>(0, 0, (sizeof(constants::acs::Id_values) / sizeof(constants::acs::Id_values[0])) - 1, 0x2506);
         SFRField<uint32_t> Kd_index = SFRField<uint32_t>(0, 0, (sizeof(constants::acs::Kd_values) / sizeof(constants::acs::Kd_values[0])) - 1, 0x2507);
         SFRField<uint32_t> Kp_index = SFRField<uint32_t>(0, 0, (sizeof(constants::acs::Kp_values) / sizeof(constants::acs::Kp_values[0])) - 1, 0x2508);
